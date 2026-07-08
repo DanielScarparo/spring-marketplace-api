a1da97b380458e3d792243f16bf272059bce210b
DanielScarparo <danielalves.scarparo@gmail.com>
2026-07-08 15:06:38 -0300

    feat: criando o terceiro banco de dados e configurando

---
+++ src/main/java/dio/marketplace/catalog/infrastructure/persistence/repository/EventMetadataEntityRepository.java
@@ -0,0 +1,9 @@
+package dio.marketplace.catalog.infrastructure.persistence.repository;
+
+import dio.marketplace.catalog.infrastructure.persistence.entity.EventMetadata;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface EventMetadataEntityRepository extends MongoRepository<EventMetadata, String> {
+}
